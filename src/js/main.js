ea2dc5c1d01b9957c9475849e346df37d5a3d859
Will King <contact@wking.io>
2019-06-12 12:50:05 -0500

    WIP

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,28 @@ import '../images/sc_1.jpg';
 import '../images/sc_2.jpg';
 
 import { dom } from './modules/dom';
-import { getAttr } from './modules/attr';
-import { setupMenu } from './modules/menu';
+import { setAttr } from './modules/attr';
+// import { setupMenu } from './modules/menu';
 
-const menuToggle = dom('.menu-toggle');
-const menu = dom(`#${getAttr('aria-controls', menuToggle)}`);
+// const menuToggle = dom('.menu-toggle');
+// const menu = dom(`#${getAttr('aria-controls', menuToggle)}`);
 
-setupMenu(menu, menuToggle);
+// setupMenu(menu, menuToggle);
+
+const logo = dom('[data-logo]');
+const parent = dom('[data-fixed]');
+
+const manageHeader = bottom => () => {
+  if (bottom <= window.scrollY) {
+    setAttr('data-fixed', true, parent);
+  } else {
+    setAttr('data-fixed', false, parent);
+  }
+};
+
+if (logo && parent) {
+  const { bottom } = logo.getBoundingClientRect();
+  const handleHeader = manageHeader(bottom + window.scrollY);
+  handleHeader();
+  window.addEventListener('scroll', handleHeader);
+}
